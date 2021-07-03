2751bc75a0ef74f939a518fa7e0d96af1f0ce87f
Ranjani Anjur Venkatraman <ranjaniwhite18@gmail.com>
2021-07-03 16:38:25 -0400

    analyzed poll data

---
+++ PyPoll/main.py
@@ -0,0 +1,49 @@
+#Pypoll
+
+import os
+import csv
+
+election_csv = os.path.join("Resources","election_data.csv")
+
+Total_votes = 0
+Candidates = []
+Candidate_votes = []
+Percent_votes = []
+
+with open(election_csv , 'r') as csvfile:
+    csvreader = csv.reader(csvfile , delimiter = ',')
+    csvheader = next(csvreader)
+    for row in csvreader:
+        Total_votes += 1
+
+        if row[2] not in Candidates:
+            Candidates.append(row[2])
+            index = Candidates.index(row[2])
+            Candidate_votes.append(1)
+        else:
+            index = Candidates.index(row[2])
+            Candidate_votes[index] += 1
+
+    for votes in Candidate_votes:
+        percentage = round(((votes/Total_votes)*100),3)
+        Percent_votes.append(percentage)
+
+    maxvotes = max(Candidate_votes)
+    index = Candidate_votes.index(maxvotes)
+    winner = Candidates[index]
+
+print("Election Results") 
+print("----------------------------")
+print(f"Total Votes : {Total_votes}")
+print("----------------------------")
+for i in range(len(Candidates)):
+    print(f"{Candidates[i]}: {Percent_votes[i]}% ({Candidate_votes[i]}) ")  
+print("----------------------------")
+print(f"Winner : {winner}")
+print("----------------------------")
+
+
+
+
+
+
